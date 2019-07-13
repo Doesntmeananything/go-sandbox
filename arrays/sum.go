c484d6f6ba807b6fa0af5ae479098b4d7ddcc6c0
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-07-13 23:18:26 +0300

    Add sum functions

--- arrays/sum.go
+++ arrays/sum.go
@@ -10,3 +10,28 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+// SumAll sums the elements of passed arrays.
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+// SumAllTails sums the tails of passed arrays.
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
